d1e44da6cc7b6edb7dda19f9b6a99981524249fe
Raj Nasit <nasitraj5678@gmail.com>
2026-07-26 12:54:34 +0530

    Fix Puppeteer for Vercel

--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -1,7 +1,7 @@
 const { GoogleGenAI } = require("@google/genai")
 const { z } = require("zod")
 const { zodToJsonSchema } = require("zod-to-json-schema")
-const puppeteer = require("puppeteer-core");
+// const puppeteer = require("puppeteer-core");
 const chromium = require("@sparticuz/chromium");
 
 const ai = new GoogleGenAI({
