e4da1ce115b45a2135af3a3f8dce731fbeb92490
Raj Nasit <nasitraj5678@gmail.com>
2026-07-26 13:14:32 +0530

    Fix Url

--- server/src/app.js
+++ server/src/app.js
@@ -6,10 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+app.use(cors());
 
 const authRouter = require("./routes/auth.routes.js");
 const interviewRouter = require("./routes/interview.routes");
