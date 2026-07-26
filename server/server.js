f9c62eb8d91ebbae0586373041941714ae34a5ac
Raj Nasit <nasitraj5678@gmail.com>
2026-07-26 11:06:29 +0530

    vercel int

--- server/server.js
+++ server/server.js
@@ -1,9 +1,32 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
+const mongoose = require("mongoose")
 
 connectToDB();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
+let isConnected = false;
+
+async function connectToMongoDB(){
+    try {
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser : true,
+            useUnifiedTopology: true
+        });
+        isConnected = true;
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error connecting to MongoDB: ",error);
+    }
+}
+
+app.use((req, res, next) => {
+    if(!isConnected){
+        connectToMongoDB();
+    }
+    next();
+})
+
+// app.listen(3000, () => {
+//     console.log("Server is running on port 3000");
+// });
