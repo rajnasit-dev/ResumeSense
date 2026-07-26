3b254a476a3677c7c04e88823b6220e592891619
Raj Nasit <nasitraj5678@gmail.com>
2026-07-26 13:34:30 +0530

    Fix catch throw error

--- client/src/features/auth/services/auth.api.js
+++ client/src/features/auth/services/auth.api.js
@@ -1,49 +1,54 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "http://localhost:3000",
-    baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true
-})
+  // baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
 
 export async function register({ username, email, password }) {
-    try {
-        const response = await api.post('/api/auth/register', {
-            username, email, password
-        })
-        return response.data;
-    } catch (err) {
-        console.log(err)
-    }
+  try {
+    const response = await api.post("/api/auth/register", {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    throw err;
+  }
 }
 
 export async function login({ email, password }) {
-    try {
-        const response = await api.post("/api/auth/login", {
-            email, password
-        })
-        return response.data;
-    } catch (err) {
-        console.log(err)
-    }
+  try {
+    const response = await api.post("/api/auth/login", {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    throw err;
+  }
 }
 
 export async function logout() {
-    try {
-        const response = await api.get("/api/auth/logout")
-        return response.data
-
-    } catch (err) {
-        console.log(err)
-    }
+  try {
+    const response = await api.get("/api/auth/logout");
+    return response.data;
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    throw err;
+  }
 }
 
 export async function getMe() {
-    try {
-        const response = await api.get("/api/auth/me")
-        return response.data
-    } catch (err) {
-        console.log(err)
-    }
-
+  try {
+    const response = await api.get("/api/auth/me");
+    return response.data;
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+    throw err;
+  }
 }
